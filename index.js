9d84334ddc50ab88ef8edc88e46fec27df8b7b28
charliedowler <charliedowler15@gmail.com>
2014-11-26 19:20:45 +0000

    re-format

--- index.js
+++ index.js
@@ -13,104 +13,104 @@ var reporter = require(stylish).reporter;
 var passed = true;
 
 function executeFlow(PATH, args, callback) {
-	'use strict';
-	var command = [
-	flowBin,
-	'check',
-	'/' + path.relative('/', PATH),
-	'--json',
-	args.join(' ')].join(' ');
+  'use strict';
+  var command = [
+    flowBin,
+    'check',
+    '/' + path.relative('/', PATH),
+    '--json',
+    args.join(' ')].join(' ');
 
-	exec(command, function (err, stdout) {
-		var parsed = JSON.parse(stdout);
-		var result = {};
-		result.errors = parsed.errors.filter(function(error) {
-			error.message = error.message.filter(function(message, index) {
-				var isCurrentFile = message.path === PATH;
-				var result = false;
-				/**
-				* If flow finds an issue related to a different file
-				* it returns a separate json property along with
-				* the different file path. We can check the previous
-				* message to see if it ends with `with`, `found` or `in`, if
-				* true we know that the next error is related to this one.
-				*/
-				var lineEnding = /(with|found|in)$/;
-				var previous = error.message[index - 1];
-				if (previous) {
-					if (lineEnding.test(previous.descr)) {
-						result = previous.path === PATH;
-					}
-				}
-				if (lineEnding.test(message.descr)) {
-					var nextMessage = error.message[index + 1];
-					if (nextMessage) {
-						result = nextMessage.path === PATH;
-					}
-				}
-				return isCurrentFile || result;
-			});
-			return error.message.length > 0;
-		});
-		if (result.errors.length) {
-			passed = false;
+  exec(command, function (err, stdout) {
+    var parsed = JSON.parse(stdout);
+    var result = {};
+    result.errors = parsed.errors.filter(function (error) {
+      error.message = error.message.filter(function (message, index) {
+        var isCurrentFile = message.path === PATH;
+        var result = false;
+        /**
+         * If flow finds an issue related to a different file
+         * it returns a separate json property along with
+         * the different file path. We can check the previous
+         * message to see if it ends with `with`, `found` or `in`, if
+         * true we know that the next error is related to this one.
+         */
+        var lineEnding = /(with|found|in)$/;
+        var previous = error.message[index - 1];
+        if (previous) {
+          if (lineEnding.test(previous.descr)) {
+            result = previous.path === PATH;
+          }
+        }
+        if (lineEnding.test(message.descr)) {
+          var nextMessage = error.message[index + 1];
+          if (nextMessage) {
+            result = nextMessage.path === PATH;
+          }
+        }
+        return isCurrentFile || result;
+      });
+      return error.message.length > 0;
+    });
+    if (result.errors.length) {
+      passed = false;
       reporter(flowToJshint(result));
-		}
-		/*jshint -W030 */
-		callback && callback(result);
-	});
+    }
+    /*jshint -W030 */
+    callback && callback(result);
+  });
 }
 
 module.exports = function (options) {
-	'use strict';
-	var opts = options || {};
-	var args = [];
-	/*jshint -W030 */
-	opts.all && args.push('--all');
-	opts.weak && args.push('--weak');
-	opts.declarations && args.push('--lib ' + opts.declarations);
-	function Flow(file, enc, callback) {
-		if (file.isNull()) {
-			this.push(file);
-			return callback();
-		}
-		else if (file.isStream()) {
-			this.emit('error',
-			new gutil.PluginError('gulp-flow', 'Stream content is not supported'));
-			return callback();
-		}
-		else if (file.isBuffer()) {
-			var PATH = path.dirname(file.path);
+  'use strict';
+  var opts = options || {};
+  var args = [];
+  /*jshint -W030 */
+  opts.all && args.push('--all');
+  opts.weak && args.push('--weak');
+  opts.declarations && args.push('--lib ' + opts.declarations);
+  function Flow(file, enc, callback) {
+    if (file.isNull()) {
+      this.push(file);
+      return callback();
+    }
+    else if (file.isStream()) {
+      this.emit('error',
+        new gutil.PluginError('gulp-flow', 'Stream content is not supported'));
+      return callback();
+    }
+    else if (file.isBuffer()) {
+      var PATH = path.dirname(file.path);
 
-			var contents = fs.readFileSync(file.path).toString();
-			var hasFlow = /\/(\*+) *@flow *(\*+)\//ig.test(contents);
-			if (hasFlow || opts.all) {
-				var configPath = path.join(PATH, '.flowconfig');
-				if (fs.existsSync(configPath)) {
-					executeFlow(file.path, args, function() {
-						callback();
-					});
-				} else {
-					fs.writeFile(configPath, '[ignore]\n[include]', function() {
-						executeFlow(file.path, args, function() {
-							fs.unlinkSync(configPath);
-							callback();
-						});
-					});
-				}
-			}
-			else {
-				this.push(file);
-				return callback();
-			}
-			this.push(file);
-		}
-	}
+      var contents = fs.readFileSync(file.path).toString();
+      var hasFlow = /\/(\*+) *@flow *(\*+)\//ig.test(contents);
+      if (hasFlow || opts.all) {
+        var configPath = path.join(PATH, '.flowconfig');
+        if (fs.existsSync(configPath)) {
+          executeFlow(file.path, args, function () {
+            callback();
+          });
+        } else {
+          fs.writeFile(configPath, '[ignore]\n[include]', function () {
+            executeFlow(file.path, args, function () {
+              fs.unlinkSync(configPath);
+              callback();
+            });
+          });
+        }
+      }
+      else {
+        this.push(file);
+        return callback();
+      }
+      this.push(file);
+    }
+  }
 
-	return through.obj(Flow, function() {
-		if (passed) {
-			console.log(logSymbols.success + ' Flow has found 0 errors');
-		}
-		this.emit('end');
-	});
+  return through.obj(Flow, function () {
+    if (passed) {
+      console.log(logSymbols.success + ' Flow has found 0 errors');
+    }
+    this.emit('end');
+  });
 };
--- test/main.js
+++ test/main.js
@@ -3,8 +3,8 @@
 "use strict";
 
 var fs = require("fs"),
-	es = require("event-stream"),
-	should = require("should");
+  es = require("event-stream"),
+  should = require("should");
 var path = require('path');
 
 require("mocha");
@@ -12,120 +12,121 @@ require("mocha");
 delete require.cache[require.resolve("../")];
 
 var gutil = require("gulp-util"),
-	flow = require("../");
+  flow = require("../");
 
 var log = console.log;
 var stringError = false;
 var iterationError = false;
 var moduleError = false;
 
-console.log = function() {
-	Array.prototype.slice.call(arguments).forEach(function(arg) {
-		if (/string/.test(arg)) stringError = true;
-		if (/iteration/.test(arg)) iterationError = true;
-		if (/Required/.test(arg)) moduleError = true;
-	});
-	log.apply(console, arguments);
+console.log = function () {
+  Array.prototype.slice.call(arguments).forEach(function (arg) {
+    if (/string/.test(arg)) stringError = true;
+    if (/iteration/.test(arg)) iterationError = true;
+    if (/Required/.test(arg)) moduleError = true;
+  });
+  log.apply(console, arguments);
 };
 
 describe("gulp-flow", function () {
+  this.timeout(5000);
 
-	it("should produce expected file via buffer", function (done) {
+  it("should produce expected file via buffer", function (done) {
 
-		var srcFile = getFixture('hello.js');
+    var srcFile = getFixture('hello.js');
 
-		var stream = flow();
+    var stream = flow();
 
-		stream.on("error", function(err) {
-			should.exist(err);
-			done(err);
-		});
+    stream.on("error", function (err) {
+      should.exist(err);
+      done(err);
+    });
 
-		stream.on("data", function (newFile) {
-			should.exist(newFile);
-			should.exist(newFile.contents);
-		});
+    stream.on("data", function (newFile) {
+      should.exist(newFile);
+      should.exist(newFile.contents);
+    });
 
-		stream.on('end', function() {
-			setTimeout(function() {
-				should.equal(stringError, true);
-				should.equal(iterationError, true);
-				done();
-			}, 500);
-		});
-		stream.write(srcFile);
-		stream.end();
-	});
+    stream.on('end', function () {
+      setTimeout(function () {
+        should.equal(stringError, true);
+        should.equal(iterationError, true);
+        done();
+      }, 500);
+    });
+    stream.write(srcFile);
+    stream.end();
+  });
 
-	it("should error on stream", function (done) {
+  it("should error on stream", function (done) {
 
-		var srcFile = new gutil.File({
-			path: "test/fixtures/hello.js",
-			cwd: "test/",
-			base: "test/fixtures",
-			contents: fs.createReadStream("test/fixtures/hello.js")
-		});
+    var srcFile = new gutil.File({
+      path: "test/fixtures/hello.js",
+      cwd: "test/",
+      base: "test/fixtures",
+      contents: fs.createReadStream("test/fixtures/hello.js")
+    });
 
-		var stream = flow();
+    var stream = flow();
 
-		stream.on("error", function(err) {
-			should.exist(err);
-			done();
-		});
+    stream.on("error", function (err) {
+      should.exist(err);
+      done();
+    });
 
-		stream.on("data", function (newFile) {
-			newFile.contents.pipe(es.wait(function(err, data) {
-				done(err);
-			}));
-		});
+    stream.on("data", function (newFile) {
+      newFile.contents.pipe(es.wait(function (err, data) {
+        done(err);
+      }));
+    });
 
-		stream.write(srcFile);
-		stream.end();
-	});
+    stream.write(srcFile);
+    stream.end();
+  });
 
-	it("should able to check with declarations", function (done) {
-		assertFile(getFixture('declaration.js'), {}, function() {
-			should.equal(moduleError, true);
-			moduleError = false;
-			assertFile(getFixture('declaration.js'), {
-				declarations: './test/fixtures/interfaces'
-			}, function() {
-				should.equal(moduleError, false);
-				done();
-			});
-		});
+  it("should able to check with declarations", function (done) {
+    assertFile(getFixture('declaration.js'), {}, function () {
+      should.equal(moduleError, true);
+      moduleError = false;
+      assertFile(getFixture('declaration.js'), {
+        declarations: './test/fixtures/interfaces'
+      }, function () {
+        should.equal(moduleError, false);
+        done();
+      });
+    });
 
-	});
+  });
 
-	function getFixture(name) {
-		var _path = '/' + path.relative('/', 'test/fixtures/' + name);
-		return new gutil.File({
-			path: _path,
-			cwd: "test/",
-			base: "test/fixtures",
-			contents: fs.readFileSync(_path)
-		});
-	}
+  function getFixture(name) {
+    var _path = '/' + path.relative('/', 'test/fixtures/' + name);
+    return new gutil.File({
+      path: _path,
+      cwd: "test/",
+      base: "test/fixtures",
+      contents: fs.readFileSync(_path)
+    });
+  }
 
-	function assertFile(srcFile, flowOptions, callback) {
-		var stream = flow(flowOptions);
-		stream.on("error", function(err) {
-			should.exist(err);
-			callback(err);
-		});
+  function assertFile(srcFile, flowOptions, callback) {
+    var stream = flow(flowOptions);
+    stream.on("error", function (err) {
+      should.exist(err);
+      callback(err);
+    });
 
-		stream.on("data", function (newFile) {
-			should.exist(newFile);
-			should.exist(newFile.contents);
-		});
+    stream.on("data", function (newFile) {
+      should.exist(newFile);
+      should.exist(newFile.contents);
+    });
 
-		stream.on('end', function() {
-			setTimeout(function() {
-				callback();
-			}, 500);
-		});
-		stream.write(srcFile);
-		stream.end();
-	}
+    stream.on('end', function () {
+      setTimeout(function () {
+        callback();
+      }, 500);
+    });
+    stream.write(srcFile);
+    stream.end();
+  }
 
 });
